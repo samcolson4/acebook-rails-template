c6dc671262cfd5f4010275069ebc9c36e6072a49
ollie beney <ombeney@gmail.com>
2020-10-20 15:43:46 +0100

    add login verification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,25 @@
 class SessionsController < ApplicationController
   def new
+  
   end
 
   def create
+    @user = User.find_by(email: params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id]= @user.id
+      
+      redirect_to '/welcome'
+    else
+      redirect_to '/login'
+    end
   end
 
   def login
+
   end
 
   def welcome
+    p current_user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6, maximum: 10 }
 end
