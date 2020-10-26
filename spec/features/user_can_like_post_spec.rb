163476da18364ac7742ffc0b7db6b43ceb3053ce
samcolson4 <samcolson4@gmail.com>
2020-10-26 12:23:18 +0000

    Refactor to use emoji

--- spec/features/user_can_like_post_spec.rb
+++ spec/features/user_can_like_post_spec.rb
@@ -4,9 +4,12 @@ feature 'like post' do
   scenario 'A user can like a post and see the number of likes go up' do
     sign_up
     make_post
-    expect(page).to have_selector(:link_or_button, 'Like')    
-    click_button("Like")
+    expect(page).to have_selector(:link_or_button, '👍')    
+    click_button("👍")
     expect(page).to have_content("1 like")
   end
 
+  # scenario 'A user can only like a post once' do
+
+  # end
 end
