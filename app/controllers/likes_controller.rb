0d0e4b7620afc6c41cf44667d48a78280537d112
samcolson4 <samcolson4@gmail.com>
2020-10-26 15:55:20 +0000

    Update like create method to also destroy

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,23 @@
 class LikesController < ApplicationController
 
   def create
-    @like = Like.create(post_id: params[:post_id], user_id: current_user.id )
+    if !(already_liked?)
+      @like = Like.create(post_id: params[:post_id], user_id: current_user.id)
+    else
+      @like = Like.find_by(user_id: current_user.id)
+      @like.destroy
+    end
     redirect_to root_url
+  end
+
+  private
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id:
+    params[:post_id]).exists?
+  end
+
+  def find_post
+    @like = Like.find(params[:post_id])
   end
 
 end
