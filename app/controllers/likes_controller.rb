a33865dc28dff2fc09e1a53ee14f27d75a6f1ca4
farhaan-ali <fa8g12@gmail.com>
2020-10-26 12:11:17 +0000

    likes storing on database now

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,7 @@
 class LikesController < ApplicationController
-  before_action :find_post
-
-  def find_post
-    @post = Post.find(params[:post_id])
-  end
 
   def create
-    p @post
-    @post.likes.create(user_id: current_user)
-    #Like.create(post_id: params[:post_id], user_id: current_user )
+    @like = Like.create(post_id: params[:post_id], user_id: current_user.id )
     redirect_to root_url
   end
 
