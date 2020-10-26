18b8f6d067d025ef05db688ea64a5f7ef4448148
farhaan-ali <fa8g12@gmail.com>
2020-10-26 11:46:05 +0000

    trying to get likes to show in database through spiking still:

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,15 @@
 class LikesController < ApplicationController
+  before_action :find_post
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    p @post
+    @post.likes.create(user_id: current_user)
+    #Like.create(post_id: params[:post_id], user_id: current_user )
+    redirect_to root_url
+  end
+
 end
---
+++ spec/controllers/likes_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe LikesController, type: :controller do
+
+  describe "POST /" do
+    it "add a like to a post" do
+      User.create(name: "Bob", email: "bob@bob.com", password: "1234567")
+      user = User.find_by(name: "Bob")
+      allow(User).to receive(:find_by).and_return(user)
+      Post.create(message: "Hello, world!", user_id: user.id )
+      post = Post.find_by(message: "Hello, world!")
+      allow(Post).to receive(:find).and_return(post.id)
+      post post_likes_path, params: { post_id: post.id, user_id: user.id }
+    end
+  end
+
+end
