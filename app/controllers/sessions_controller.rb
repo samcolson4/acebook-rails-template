52285063b5ffb5179dd69ea722274ab34fdb041f
Oliver Beney <62512189+obean@users.noreply.github.com>
2020-10-26 14:22:21 +0000

    Logoutflash (#18)
    * removed the auto fill in new post
    
    * autofill fixed
    
    * added break lines to the post message
    
    * flash messages for invalid login
    
    Co-authored-by: courtenay donald <co.donald18@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if User.find_by(email: params[:email])
     @user = User.find_by(email: params[:email])
-
-    if @user && @user.authenticate(params[:password])
-      session[:user_id]= @user.id
-      redirect_to '/'
+      if @user && @user.authenticate(params[:password])
+        session[:user_id]= @user.id
+        redirect_to '/'
+      else
+        flash[:invalid_password] = "Password is invalid  "
+        redirect_to '/login'
+      end
     else
+      flash[:invalid_email] = "Email is invalid"
       redirect_to '/login'
     end
   end
