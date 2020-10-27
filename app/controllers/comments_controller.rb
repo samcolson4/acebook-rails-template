a919fae18f721e19c94dfa5b322d3a04961330b4
courtenay donald <co.donald18@gmail.com>
2020-10-27 17:05:12 +0000

    upadate comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,26 @@ class CommentsController < ApplicationController
    redirect_to root_path
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+
+    if current_user.id == @comment.user_id
+      if @comment.update(comment_params)
+        redirect_to root_path
+      else  
+        render 'edit'
+      end
+    else 
+      redirect_to root_path, :status => 403
+    end
+  end
+
   private
   def comment_params  
     params.require(:comment).permit(:body, :user_id)
