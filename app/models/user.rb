67e5e72d1eb1eb334cbe1711a121cfbe842aa375
Sam Colson <samcolson4@gmail.com>
2020-10-27 10:17:45 +0000

    Likes (#21)
    * setup model and controller for likes
    
    * trying to get likes to show in database through spiking still:
    
    * likes storing on database now
    
    * Add like feature test and counting number of likes
    
    * Refactor to use emoji
    
    * Add like feature test
    
    * Update like create method to also destroy
    
    * Refactor
    
    * delete coverage files
    
    * Update user_can_like_post_spec.rb
    
    Co-authored-by: farhaan-ali <fa8g12@gmail.com>
    Co-authored-by: Farhaan <59924029+farhaan-ali@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :likes, dependent: :destroy
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
---
+++ spec/features/user_can_like_post_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'like post' do
+  scenario 'A user can like a post and see the number of likes go up' do
+    sign_up
+    make_post
+    expect(page).to have_selector(:link_or_button, '👍')    
+    click_button("👍")
+    expect(page).to have_content("1 like")
+  end
+
+  scenario 'A user can only like a post once and unlike it' do
+    sign_up
+    make_post
+    click_button("👍")
+    expect(page).to have_content("1 like")
+    click_button("👍")
+    expect(page).to have_content("0 likes")
+  end
+end
