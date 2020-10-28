5022e2c0824cf14f5d465b0ad1765a6a35d1ddce
courtenay donald <co.donald18@gmail.com>
2020-10-28 10:42:03 +0000

    update edit feature test

---
+++ spec/features/user_can_delete_and_edit_comment_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature "comments" do 
+    scenario "user can delete comment" do 
+      sign_up_as_potato
+      make_post
+      fill_in "comment[body]", with: "First comment"
+      click_button "Create Comment"
+      click_link "Destroy Comment"
+      expect(page).not_to have_content "First comment"
+    end
+
+    scenario "user can edit their own comment" do 
+        sign_up_as_potato
+        make_post
+        fill_in "comment[body]", with: "First comment"
+        click_button "Create Comment"
+        click_link "Edit Comment"
+        fill_in "comment[body]", with: "Second comment"
+        click_button "Update Comment"
+        expect(page).to have_content "Second comment"
+        expect(page).not_to have_content "First comment"
+    end
+
+end 
