20fa84077846865a02f8e2977a84bd1484e58f97
samcolson4 <samcolson4@gmail.com>
2020-10-29 14:24:17 +0000

    All tests passing

--- spec/features/user_can_delete_and_edit_comment_spec.rb
+++ spec/features/user_can_delete_and_edit_comment_spec.rb
@@ -6,7 +6,8 @@ feature "comments" do
       make_post
       fill_in "comment[body]", with: "First comment"
       click_button "comment"
-      click_link "delete"
+      # click_link "delete"
+      find('.delete_edit').click_link "delete"
       expect(page).to have_current_path "/"
       expect(page).not_to have_content "First comment"
     end
@@ -18,19 +19,18 @@ feature "comments" do
       make_post
       fill_in "comment[body]", with: "First comment"
       click_button "comment"
-      # TODO: link names need to be refactored
-      click_link "delete"
+      find('.delete_edit').click_link "delete"
       expect(page).to have_current_path "/users/#{user.id}"
       expect(page).not_to have_content "First comment"
     end
 
     scenario "user can edit their own comment" do 
         sign_up_as_potato
+        user = User.find_by(name: "Potato")
         make_post
         fill_in "comment[body]", with: "First comment"
         click_button "comment"
-        allow(Time).to receive(:now).and_return(Time.now + 700)
-        click_link "edit"
+        find('.comment').click_link "edit"
         fill_in "comment[body]", with: "Second comment"
         click_button "Update Comment"
         expect(page).to have_content "Second comment"
