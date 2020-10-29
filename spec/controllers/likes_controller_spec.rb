e77e63acdac3b8c542e65d4b4dada8c3e110d37f
Veronica Lee <veronicavlee@icloud.com>
2020-10-29 15:27:44 +0000

    Add controller tests for Likes :create

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -1,17 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe LikesController, type: :controller do
+describe LikesController do
+  describe "POST /" do
+    it "adds a like to a post if not already liked by user" do
+      User.create(name: "Bob", email: "bob@bob.com", password: "1234567")
+      user = User.find_by(name: "Bob")
+      allow(User).to receive(:find_by).and_return(user)
+      Post.create(message: "Hello, world!", user_id: user.id, posted_to_id: user.id )
+      my_post = Post.find_by(message: "Hello, world!")
+      post :create, params: { post_id: my_post.id, user_id: user.id }
+      expect(Like.first).to be
+    end
+    
+    it "removes like from post if already liked by user" do
+      User.create(name: "Bob", email: "bob@bob.com", password: "1234567")
+      user = User.find_by(name: "Bob")
+      allow(User).to receive(:find_by).and_return(user)
+      Post.create(message: "Hello, world!", user_id: user.id, posted_to_id: user.id )
+      my_post = Post.find_by(message: "Hello, world!")
+      post :create, params: { post_id: my_post.id, user_id: user.id }
 
-  # describe "POST /" do
-  #   it "add a like to a post" do
-  #     User.create(name: "Bob", email: "bob@bob.com", password: "1234567")
-  #     user = User.find_by(name: "Bob")
-  #     allow(User).to receive(:find_by).and_return(user)
-  #     Post.create(message: "Hello, world!", user_id: user.id )
-  #     post = Post.find_by(message: "Hello, world!")
-  #     allow(Post).to receive(:find).and_return(post.id)
-  #     post post_likes_path, params: { post_id: post.id, user_id: user.id }
-  #   end
-  # end
+      post :create, params: { post_id: my_post.id, user_id: user.id }
+      expect(Like.first).not_to be
+    end
+
+    it "redirects back to the page" do
+      User.create(name: "Bob", email: "bob@bob.com", password: "1234567")
+      user = User.find_by(name: "Bob")
+      allow(User).to receive(:find_by).and_return(user)
+      Post.create(message: "Hello, world!", user_id: user.id, posted_to_id: user.id )
+      my_post = Post.find_by(message: "Hello, world!")
+      post :create, params: { post_id: my_post.id, user_id: user.id }
+
+      expect(response).to have_http_status(302)
+    end
+  end
 
 end
