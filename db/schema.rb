d9c98fd2f967377c410bb98287a764972eac8310
courtenay donald <co.donald18@gmail.com>
2020-10-29 11:35:47 +0000

    cooment hide and show jquery feature

--- db/schema.rb
+++ db/schema.rb
@@ -12,19 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2020_10_27_143332) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "friends", force: :cascade do |t|
-    t.string "status", default: "pending"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "requester_id", null: false
-    t.bigint "requestee_id", null: false
-    t.index ["requestee_id"], name: "index_friends_on_requestee_id"
-    t.index ["requester_id"], name: "index_friends_on_requester_id"
-  end
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -35,6 +24,16 @@ ActiveRecord::Schema.define(version: 2020_10_27_143332) do
     t.bigint "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.string "status", default: "pending"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "requester_id", null: false
+    t.bigint "requestee_id", null: false
+    t.index ["requestee_id"], name: "index_friends_on_requestee_id"
+    t.index ["requester_id"], name: "index_friends_on_requester_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -63,10 +62,10 @@ ActiveRecord::Schema.define(version: 2020_10_27_143332) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "friends", "users", column: "requestee_id"
-  add_foreign_key "friends", "users", column: "requester_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "friends", "users", column: "requestee_id"
+  add_foreign_key "friends", "users", column: "requester_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
